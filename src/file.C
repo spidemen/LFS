eafafb346ae8ef7f20f59b914fdbfe596cbac725
spidemen <xingyuan@email.arizona.edu>
2019-03-07 13:19:09 -0700

    update

--- src/file.C
+++ src/file.C
@@ -4,11 +4,10 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
+#include <flash.h>
 static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 static const char *link_path = "/link";
-
 
 int main(int argc, char *argv[])
 {

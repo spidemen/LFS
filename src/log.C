dec308ba107b38f66352c31e9a915733de7d54cf
spidemen <xingyuan@email.arizona.edu>
2019-03-16 12:38:00 -0700

    update

--- src/log.C
+++ src/log.C
@@ -372,16 +372,16 @@ void TestLogWrite(){
 	 cout<<"2nd read from flash. segmentNo="<<address.segmentNo<<"  "<<readbuf<<endl;
 }
 
-// int main(int argc, char *argv[])
-// {
-// 	 cout<<"hell World"<<endl;
-//      init("FuseFileSystem");
-//    //  testWrite("This is the first time to use flash libraray 1 l\n",2);
-//    //  testWrite("This is the first time to use flash libraray 2 l\n",2);
-//  //    testWrite("This is the first time to use flash libraray 3 l\n",4);
-//  //    testWrite("This is the first time to use flash libraray 4 l\n",6);
-//  //    testRead(0);
-//       TestLogWrite();
-// 	//  delete segmentCache;
-//     return 1;
-// }
+int main(int argc, char *argv[])
+{
+	 cout<<"hell World"<<endl;
+     init("FuseFileSystem");
+   //  testWrite("This is the first time to use flash libraray 1 l\n",2);
+   //  testWrite("This is the first time to use flash libraray 2 l\n",2);
+ //    testWrite("This is the first time to use flash libraray 3 l\n",4);
+ //    testWrite("This is the first time to use flash libraray 4 l\n",6);
+ //    testRead(0);
+      TestLogWrite();
+	//  delete segmentCache;
+    return 1;
+}

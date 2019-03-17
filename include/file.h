76f731eaace09aa52f28787c14ee1df7856c17f5
Katy Williams <kawilliams@email.arizona.edu>
2019-03-16 20:17:19 -0700

    Make clean

--- include/file.h
+++ include/file.h
@@ -5,24 +5,36 @@
 #ifndef file_h
 #define file_h
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include  "log.h"
 
-//static const int FNAME_LEN = 50;
+
+
+// struct IfileAddress {
+//     vector<logAddress> addresses; 
+// };
+
+
+// struct IfileLocation {
+//     u_int mostRecentSeg;
+//     u_int mostRecentBlock;
+// };
+
 
 struct Inode {
-    int inum;           // in spec
-    char *filename;     // in spec
-    int type;           // in spec -- 0 for file, 1 for directory
-    u_int size;         // in spec
-    u_int block_number; // in spec -- flash addresses of file's blocks
+    int inum = 0;           // in spec
+    string filename = "";     // in spec
+    int type = 0;           // in spec -- 0 for file, 1 for directory
+    u_int size = 0;         // in spec
+    u_int block_number = 0; // in spec -- flash addresses of file's blocks
     
-    int in_use; 	// true 1, false 0
+    int in_use = 0; 	// true 1, false 0
     //time_t time_of_last_change;
     struct tm * time_of_last_change;
-    char* owner;
-    int permissions;
-    u_int offset;
+    string owner = "user";
+    int permissions = 0;
+    u_int offset = 0;
 
     struct logAddress *Block1Ptr;
     struct logAddress *Block2Ptr;
@@ -30,6 +42,14 @@ struct Inode {
     struct logAddress *Block4Ptr;
     struct logAddress *OtherBlocksPtr;
 };
+
+//static const int FNAME_LEN = 50;
+struct Ifile {
+    int inum = 0;
+    vector<Inode> data; //location of Inode == inum, should be vector<Inode>
+    vector<logAddress> addresses; 
+};
+
 
 
 

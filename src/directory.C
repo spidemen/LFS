dc8269b108155dd8e1ff6e8258983947ca3eded3
spidemen <xingyuan@email.arizona.edu>
2019-03-17 00:36:57 -0700

    remove hello.o

--- src/directory.C
+++ src/directory.C
@@ -8,18 +8,10 @@ int directoryInit(){
 
    }
 }
-int 
-
 int main(int argc, char *argv[])
 {
 	 cout<<"hell World"<<endl;
-     init("FuseFileSystem");
-   //  testWrite("This is the first time to use flash libraray 1 l\n",2);
-   //  testWrite("This is the first time to use flash libraray 2 l\n",2);
- //    testWrite("This is the first time to use flash libraray 3 l\n",4);
- //    testWrite("This is the first time to use flash libraray 4 l\n",6);
- //    testRead(0);
-      TestLogWrite();
+   	
 	//  delete segmentCache;
     return 1;
 }

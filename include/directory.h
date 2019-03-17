4be8d6548ad8df59ed6911dceacf7c0c68aa7852
spidemen <xingyuan@email.arizona.edu>
2019-03-17 00:27:03 -0700

    add directory

---
+++ include/directory.h
@@ -0,0 +1,26 @@
+#ifndef _DIRECTORY_H
+#define _DIRECTORY_H
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <flash.h>
+#include  "log.h"
+#include  "file.h"
+
+#define IFILE 0
+
+typedef string FileName
+
+vector<pair<FileName,inum> > tables;
+
+int inodeSize=144;   // default vaule
+
+int currentinum=1;
+
+#endif

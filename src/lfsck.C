b70efbfe98179c55f2fb08a33916ed7edfe9625a
Katy Williams <kawilliams@email.arizona.edu>
2019-03-18 15:13:22 -0700

    Take some Blocks and write as JSON format to memory.json, talk to Python

--- src/lfsck.C
+++ src/lfsck.C
@@ -12,51 +12,79 @@
 
 int totalsectors = 2;
 
-/* Convert LFS data structures into JSON */
-int convertToJSON( void *buffer ) {
 
-	printf("convertToJSON\n");
-	printf("%s\n", (char *)buffer);
+int callPythonJson(PyObject *pDict, PyObject *pFunc, PyObject *pArgs, PyObject *pValue) {
+
+	//Get the add method from the dictionary
+ 	pFunc = PyDict_GetItemString(pDict, "parseJsonFile");
+ 	if (pFunc == NULL) {
+ 		cout << "couldn't get parseJSON function" <<endl;
+ 	}
+ 	//Create tuple for the args for add()
+ 	pArgs = PyTuple_New(2); 
+ 	pValue = PyInt_FromLong(2);
+ 	PyTuple_SetItem(pArgs,0,pValue);
+ 	PyTuple_SetItem(pArgs,1,pValue);
+
+ 	PyObject *pResult = PyObject_CallObject(pFunc, pArgs);
+ 	 if (pResult==NULL){
+ 		printf("Calling the parseJSON method failed.\n");
+ 		return 1;
+ 	}
+
+ 	char* result = PyString_AsString(pResult);
+  	
+  	printf("Outcome of parseJSON: %s.\n", result);
+
+}
+
+
+/* Convert LFS data structures into JSON */
+int convertToJSON( Block *b, FILE* jsonfile) {
+
+	int STR_SIZE =  sizeof(*b);
+	char blockString[STR_SIZE];
+	sprintf(blockString, "{'blockNo' :' %d', 'aLive' : '%d', 'blockUse' : '%d', 'data' : '%s', 'offset' : '%d'}", b->blockNo, b->aLive, b->blockUse, b->data, b->offset);
+
+	//cout << blockString << endl;
+	fputs(blockString, jsonfile);
+	
 	return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-	PyObject *pName, *pModule, *pDict, *pFunc;
-	PyObject *pArgs, *pValue;
+	PyObject *pName, *pModule, *pDict, *pFunc, *pArgs, *pValue, *pValue1, *pValue2;
 	Py_Initialize();
  	PyRun_SimpleString("from time import time,ctime\n"
                      "print 'Today is',ctime(time())\n");
+ 	PyRun_SimpleString("import sys");
+ 	PyRun_SimpleString("import os");
+ 	PyRun_SimpleString("sys.path.append(os.getcwd())");
 
  	// Import the JsonParser as Python module
  	pName = PyString_FromString("parseJson");
+ 	if (pName == NULL) {
+ 		printf("Error parsing string\n");
+ 		return 1;
+ 	}
+
  	pModule = PyImport_Import(pName);
+ 	if (pModule == NULL) {
+ 		printf("Error with pModule and importing parseJson.py\n");
+ 		return 1;
+ 	}
+ 	Py_DECREF(pName);
  	//Create a dicitonary for the contents of the module
  	pDict = PyModule_GetDict(pModule);
- 	//Get tehe add method from the dictionary
- 	/*pFunc = PyDict_GetItemString(pDict, "add");
- 	//Create tuple for the args for add()
- 	pArgs = PyTuple_New(2); 
- 	//Convert 2 to a python int
- 	pValue = PyInt_FromLong(2);
- 	// Set the python int as the 1st and 2nd args 
- 	PyTuple_SetItem(pArgs,0,pValue);
- 	PyTuple_SetItem(pArgs,1,pValue);
 
- 	//Call the fucntion with the args
- 	PyObject *pResult = PyObject_CallObject(pFunc,pArgs);
- 	if (pResult==NULL){
- 		printf("Calling the add method failed.\n");
- 	}
- 	long result = PyInt_AsLong(pResult);
-  	Py_Finalize();
-  	printf("The result is %d.\n", result);*/
-  
+ 	// Test to check if python communication is working
+  	//pythonTest(pDict, pFunc, pArgs, pValue1, pValue2);
 
 	if ( argc != 2 ) /* argc should be 2 for correct execution */
     {
-        /* We print argv[0] assuming it is the program name */
+        //We print argv[0] assuming it is the program name 
         printf( "Provide filename: %s <filename>", argv[0]);
     }
     else 
@@ -66,7 +94,6 @@ int main(int argc, char *argv[])
         char* file = argv[1];
         u_int blocks;
         Flash f = Flash_Open(file, FLASH_ASYNC, &blocks);
-        /* fopen returns 0, the NULL pointer, on failure */
         if ( f == NULL )
         {
             printf( "Could not open file\n" );
@@ -79,7 +106,9 @@ int main(int argc, char *argv[])
         	// printf("So FLASH_BLOCK_SIZE is %d\n", FLASH_BLOCK_SIZE);
         	u_int sector = 0;
         	u_int count = 1;
-     
+     		FILE *jsonfile;
+     		jsonfile = fopen("memory.json", "w");
+     		fputs("{", jsonfile);
 
  			// * Flash_Read reads "count" sectors from "flash" into "buffer" 
  			// * starting at sector "sector".
@@ -98,19 +127,27 @@ int main(int argc, char *argv[])
 	            	
 	            	//cout <<"Buffer contents"<<buffer<<endl;
 	            	SegmentSummary *summary = (SegmentSummary *)buf;
-	            	cout <<"SegmentSummary segmentNo = "<< summary->segmentNo<<endl;
-	            	cout <<"SegmentSummary inUse = "<<summary->inUse<<endl;
-	            	cout <<" modifiedTime = "<<summary->modifiedTime<<endl;
-
-	    			
-	            	//convertToJSON( buffer );
+	            	if (summary->segmentNo != 0) {
+	            		cout <<"SegmentSummary segmentNo = "<< summary->segmentNo<<endl;
+	            		cout <<"SegmentSummary inUse = "<<summary->inUse<<endl;
+	            		cout <<" modifiedTime = "<<summary->modifiedTime<<endl;
+	            	}
+	    			Block *d = (Block *)buf;
+	    			cout << "Block no"<<d->blockNo << "| " <<d->aLive<< "| " << d->blockUse << "| "<<d->offset <<endl;
+	            	convertToJSON( d , jsonfile);
 	            }
         	}
+        	
+        	fputs("}", jsonfile);
+        	fclose(jsonfile);
+
+            callPythonJson( pDict, pFunc,  pArgs , pValue);
             
-            
-           Flash_Close(f);
+           	Flash_Close(f);
         }
     }
+
+    Py_Finalize();
     return 0;
 } 
 
